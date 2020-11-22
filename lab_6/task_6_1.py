7801cd3f2109bd8c8d393fc4a75b8158493eb612
andr <donrunderstend@mail.ru>
2020-11-22 12:36:14 +0100

    lab_6 task_6_1 aded square, movement square

--- lab_6/task_6_1.py
+++ lab_6/task_6_1.py
@@ -3,6 +3,7 @@
 """
 
 import pygame
+import math
 from pygame.draw import *
 from random import randint
 
@@ -20,22 +21,59 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 FPS = 1
+DISP_X = 800
+DISP_Y = 600
 
 x, y, r = -1, -1, -1
 lst_balls = list()
+lst_rec = list()
 my_font = pygame.font.SysFont('Sans bolt', 130)
 
 
-def new_ball():
+def roll_fig(balls, way):
+    """Movement and reflection of goals"""
+    bl_lst = list()
+    for ball in balls:
+        bl_angle = ball[-1]
+        bl_x = ball[1][0]
+        bl_y = ball[1][1]
+        bl_r = ball[2]
+        print(bl_angle)
+        if bl_x + bl_r > DISP_X - way:
+            bl_x = DISP_X - bl_r
+            bl_angle = randint(180, 360)
+            way *= 2
+        elif bl_x - bl_r < way:
+            bl_x = bl_r
+            bl_angle = randint(0, 180)
+            way *= 2
+        elif bl_y + bl_r > DISP_Y - way:
+            bl_y = DISP_Y - bl_r
+            bl_angle = randint(90, 270)
+            way *= 2
+        elif bl_y - bl_r < way:
+            bl_y = bl_r
+            bl_angle = randint(-90, 90)
+            way *= 2
+
+        bl_x += way * math.cos(bl_angle * math.pi / 180)
+        bl_y += way * math.sin(bl_angle * math.pi / 180)
+        print(bl_angle)
+        bl_lst.append((ball[0], (bl_x, bl_y), bl_r, bl_angle))
+    return bl_lst
+
+
+def new_fig(lst):
     """Draw new ball"""
     global x, y, r
     x = randint(100, 700)
     y = randint(100, 500)
     r = randint(50, 100)
+    angle = randint(0, 360)
 
     color = COLORS[randint(0, 5)]
-    lst_balls.append((color, (x, y), r))
-    # circle(screen, color, (x, y), r)
+    lst.append((color, (x, y), r, angle))
+    return lst
 
 
 def click(event_click, balls):
@@ -56,34 +94,44 @@ def click(event_click, balls):
 
 
 def show_score(score_show):
+    """Display score"""
     text_surface = my_font.render(str(score_show), False, (0, 0, 0))
     screen.blit(text_surface, (20, 10))
 
 
-def display_update(score_sh, balls):
+def display_update(score_sh, balls, squares):
     screen.fill((230, 230, 230))
     show_score(score_sh)
     for i in balls:
-        circle(screen, *i)
+        circle(screen, *i[:-1])
+
+    for square in squares:
+        side = square[2]
+        ring_r = side / 2
+        rect(screen, square[0],
+             (square[1][0] - ring_r, square[1][1] - ring_r, side, side))
 
     pygame.display.update()
 
 
 # Create a window
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((DISP_X, DISP_Y))
 screen.fill((230, 230, 230))
 pygame.display.set_caption('Catch the ball')
 
 
 score = 0
-display_update(score, lst_balls)
+display_update(score, lst_balls, lst_rec)
 
 # Create object clock for creating delay
 clock = pygame.time.Clock()
 # Create main loop
 finished = False
 time = 0
-new_ball()
+
+speed = 50
+
+new_fig(lst_balls)
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
@@ -95,12 +143,19 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if click(event.pos, lst_balls):
                 score += 1
-                display_update(score, lst_balls)
-    if time >= 10:
-        new_ball()
+                display_update(score, lst_balls, lst_rec)
+            if click(event.pos, lst_rec):
+                score += 2
+                display_update(score, lst_balls, lst_rec)
+    lst_balls = roll_fig(lst_balls, speed)
+    lst_rec = roll_fig(lst_rec, speed)
+    if time == 5:
+        lst_rec = new_fig(lst_rec)
+    elif time >= 10:
+        lst_balls = new_fig(lst_balls)
         time = 0
     time += 1
     print(time)
-    display_update(score, lst_balls)
+    display_update(score, lst_balls, lst_rec)
 
 pygame.quit()
